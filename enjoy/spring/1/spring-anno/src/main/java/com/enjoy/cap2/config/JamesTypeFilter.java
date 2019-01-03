1b3115b46672a7c6c17746973a1d637b17b62791
eangulee <mbtjay-2004@qq.com>
2019-01-03 09:58:44 +0800

    fixed

--- enjoy/spring/1/spring-anno/src/main/java/com/enjoy/cap2/config/JamesTypeFilter.java
+++ enjoy/spring/1/spring-anno/src/main/java/com/enjoy/cap2/config/JamesTypeFilter.java
@@ -1,4 +1,4 @@
-package com.enjoy.cap2.config;
+﻿package com.enjoy.cap2.config;
 
 import java.io.IOException;
 
@@ -13,23 +13,23 @@ public class JamesTypeFilter implements TypeFilter{
 	private ClassMetadata classMetadata;
 
 	/*
-	 * MetadataReader:��ȡ����ǰ����ɨ�������Ϣ
-	 * MetadataReaderFactory:���Ի�ȡ�������κ�����Ϣ
+	 * MetadataReader:读取到当前正在扫描类的信息
+	 * MetadataReaderFactory:可以获取到其他任何类信息
 	 */
 	
 	@Override
 	public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory)
 			throws IOException {
-		//��ȡ��ǰ��ע�����Ϣ
+		//获取当前类注解的信息
 		AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
-		//��ȡ��ǰ����ɨ�������Ϣ
+		//获取当前正在扫描的类信息
 		classMetadata = metadataReader.getClassMetadata();
-		//��ȡ��ǰ����Դ(���·��)
+		//获取当前类资源(类的路径)
 		Resource resource = metadataReader.getResource();
 		
 		String className = classMetadata.getClassName();
 		System.out.println("----->"+className);
-		if(className.contains("Order")){//�������er�ַ�, ��ƥ��ɹ�,����true
+		if(className.contains("order")){//当类包含er字符, 则匹配成功,返回true
 			return true;
 		}
 		return false;
