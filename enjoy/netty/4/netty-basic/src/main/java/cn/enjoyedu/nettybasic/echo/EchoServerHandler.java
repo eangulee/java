20fd465507dd50c271c9d96da8b1dd93900ac647
eangulee <eangulee@gmail.com>
2019-07-21 18:18:00 +0800

    netty

---
+++ enjoy/netty/4/netty-basic/src/main/java/cn/enjoyedu/nettybasic/echo/EchoServerHandler.java
@@ -0,0 +1,10 @@
+package cn.enjoyedu.nettybasic.echo;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 类说明：自己的业务处理
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+}
